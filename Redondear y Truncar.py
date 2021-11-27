195204b87460aaea4506a3f5842d509a6ba923a8
Indev-Error <86646025+IndevError@users.noreply.github.com>
2021-11-26 23:10:10 -0600

    Add files via upload

---
+++ Redondear y Truncar.py
@@ -0,0 +1,14 @@
+#Libreria
+import math
+
+#Recibir datos
+Numero = float(input("Ingresa un Numero: "))
+
+
+#Mostrar datos
+
+#Redondear
+print(math.ceil(Numero*10000)/10000)
+
+#Truncar
+print(math.floor(Numero*10000)/10000)
